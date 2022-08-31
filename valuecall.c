a1ff67fd35310a13b1e91474503695c3ece39607
Rameshporu19 <rameshporu19@gmail.com>
2022-08-31 20:28:58 +0530

    14th

---
+++ valuecall.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+void fun(int x ,int y);
+
+int main()
+{
+	int x=5;
+	int y=7;
+	fun(x,y);
+		printf("x=%d,y=%d\n",x,y);
+	
+}
+void fun(int x, int y){
+	x = 7, y = 5;
+	printf("x=%d,y=%d\n",x,y);
+}
